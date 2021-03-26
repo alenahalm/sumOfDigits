2031032573f73678fce03318a6dd6fcb3a22dbbb
alenahalm <alenahalmatova749@gmail.com>
2021-03-26 15:45:57 +0800

    Initial commit

---
+++ sumOfDigits.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int summ(int m) {
+	if (m < 0) {
+		m *= (-1);
+	}
+	if (m < 10) {
+		return m;
+	}
+	else {
+		return summ(m / 10) + m % 10;
+	}
+}
+
+int main()
+{
+	cout << "Enter your number" << endl;
+	int m;
+	cin >> m;
+	cout << summ(m) << endl;
+}
